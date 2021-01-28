2283cd73d581d844dc981dd7f89fd07d300932a8
andre-prado <andr3prad0@gmail.com>
2021-01-28 19:01:05 -0300

    adiciona normalize-space em title e titlte_url

--- timeline/timeline/spiders/globo_news.py
+++ timeline/timeline/spiders/globo_news.py
@@ -9,8 +9,8 @@ class GloboNewsSpider(scrapy.Spider):
     def parse(self, response):
         data = datetime.now()
         yield {
-            "title": response.xpath("//div[@class='highlight headline']/a/@title").get(),
-            "title_url": response.xpath("//div[@class='highlight headline']/a/@href").get(),
+            "title": response.xpath("normalize-space(//div[@class='highlight headline']/a/@title)").get(),
+            "title_url": response.xpath("normalize-space(//div[@class='highlight headline']/a/@href)").get(),
             "date": data.strftime(f'{data.day}/{data.month}/{data.year}'),
             "hour": data.strftime(f"{data.hour}:{data.minute}") if data.minute >= 10 else data.strftime(f"{data.hour}:0{data.minute}")
         }
