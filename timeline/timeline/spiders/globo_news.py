57ae38e8921510171c83e733006fecea0e639cff
andre-prado <andr3prad0@gmail.com>
2021-01-28 21:57:20 -0300

    adiciona script para pegar dados de titulo com foto

--- timeline/timeline/spiders/globo_news.py
+++ timeline/timeline/spiders/globo_news.py
@@ -8,9 +8,28 @@ class GloboNewsSpider(scrapy.Spider):
 
     def parse(self, response):
         data = datetime.now()
-        yield {
-            "title": response.xpath("normalize-space(//div[@class='highlight headline']/a/@title)").get(),
-            "title_url": response.xpath("normalize-space(//div[@class='highlight headline']/a/@href)").get(),
-            "date": data.strftime(f'{data.day}/{data.month}/{data.year}'),
-            "hour": data.strftime(f"{data.hour}:{data.minute}") if data.minute >= 10 else data.strftime(f"{data.hour}:0{data.minute}")
+
+        title_sdt = response.xpath("normalize-space(//div[@class='highlight headline']/a/@title)").get()
+        title_url_sdt = response.xpath("normalize-space(//div[@class='highlight headline']/a/@href)").get()
+
+        title_photo = response.xpath("normalize-space(//div[@class='highlight photo']/a[@class='highlight__link with-picture']/@title)").get()
+        title_photo_url = response.xpath("normalize-space(//div[@class='highlight photo']/a[@class='highlight__link with-picture']/@href)").get()
+
+
+        date = data.strftime(f'{data.day}/{data.month}/{data.year}')
+        hour = data.strftime(f"{data.hour}:{data.minute}") if data.minute >= 10 else data.strftime(f"{data.hour}:0{data.minute}")
+
+        if title_sdt:
+            yield {
+                "title": title_sdt,
+                "title_url": title_url_sdt,
+                "date": date,
+                "hour": hour
+        }
+        elif title_photo:
+            yield {
+                "title": title_photo,
+                "title_url": title_photo_url,
+                "date": date,
+                "hour": hour
         }
